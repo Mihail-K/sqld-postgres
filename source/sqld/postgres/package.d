a254a86afbe9c0b31dbfec1068b0fb2d2187bc20
Mihail-K <mihail@platterz.ca>
2017-04-29 16:16:26 -0400

    Add package file.

---
+++ source/sqld/postgres/package.d
@@ -0,0 +1,7 @@
+
+module sqld.postgres;
+
+public import sqld.postgres.escape;
+public import sqld.postgres.format_literal;
+public import sqld.postgres.prepared_visitor;
+public import sqld.postgres.visitor;
