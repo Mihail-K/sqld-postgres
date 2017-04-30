4fcf36df85a86d751c64a6fa6ef48a50a61f2244
Mihail-K <mihail@platterz.ca>
2017-04-30 16:50:24 -0400

    Add tests for array literals.

--- source/sqld/postgres/types/array.d
+++ source/sqld/postgres/types/array.d
@@ -58,3 +58,28 @@ auto pgArray(T)(T elements) nothrow if(isPostgresArrayType!(T) && isPostgresArra
     assert(pgArray([[1, 2], [3, 4]]).sql == "ARRAY[ARRAY[1, 2], ARRAY[3, 4]]");
     assert(pgArray([[[1], [2]], [[3], [4]]]).sql == "ARRAY[ARRAY[ARRAY[1], ARRAY[2]], ARRAY[ARRAY[3], ARRAY[4]]]");
 }
+
+@system unittest
+{
+    import sqld.ast : eq, table;
+    import sqld.postgres.visitor : PostgresVisitor;
+
+    auto v = new PostgresVisitor;
+    auto u = table("users");
+    auto n = u["likes"].eq(pgArray(["cars", "ham", "trees"]));
+
+    n.accept(v);
+    assert(v.sql == `"users"."likes" = ARRAY['cars', 'ham', 'trees']`);
+}
+
+@system unittest
+{
+    import sqld.ast : literal;
+    import sqld.postgres.visitor : PostgresVisitor;
+
+    auto v = new PostgresVisitor;
+    auto n = literal([pgArray(["red"]), pgArray(["red", "blue"])]);
+
+    n.accept(v);
+    assert(v.sql == `(ARRAY['red'], ARRAY['red', 'blue'])`);
+}
