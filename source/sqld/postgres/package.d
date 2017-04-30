c27ee23eed4d82b820193e5a568d914c52969863
Mihail-K <mihail@platterz.ca>
2017-04-30 15:37:13 -0400

    Don't export unnecessary modules.

--- source/sqld/postgres/package.d
+++ source/sqld/postgres/package.d
@@ -1,7 +1,5 @@
 
 module sqld.postgres;
 
-public import sqld.postgres.format_literal;
 public import sqld.postgres.prepared_visitor;
-public import sqld.postgres.quote;
 public import sqld.postgres.visitor;
